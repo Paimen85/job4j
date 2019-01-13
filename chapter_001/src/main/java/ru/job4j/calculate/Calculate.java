0cff5692fbca2dfd115fd69355c545a94f3fd818
Paimen85 <evgeni.baranov@gmail.com>
2019-01-13 12:57:15 +0200

    Format text ctrl+Alt+I

--- chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
@@ -1,25 +1,25 @@
 package ru.job4j.calculate;
 /**
-* Class Calculate Рещение задачи 1.3. JavaDoc. Code convention.
-* @author ebaranov.
-* @since  09.01.2019.
-*/
+ * Class Calculate Рещение задачи 1.3. JavaDoc. Code convention.
+ * @author ebaranov.
+ * @since  09.01.2019.
+ */
 public class Calculate {
 	/**
-	* Method main 
-	* @param args - args.
-	*/
+	 * Method main
+	 * @param args - args.
+	 */
 	public static void main(String[] args) {
 		System.out.println("Hello World");
 	}
-		
+
 	/**
-	* Method echo.
-	* @param name Your name
-	* @return Echo plus your name.
-	*/
+	 * Method echo.
+	 * @param name Your name
+	 * @return Echo plus your name.
+	 */
 	public String echo(String name) {
 		return "Echo, echo, echo : " + name;
 	}
-	
+
 }
--- chapter_001/src/test/java/ru/job4j/calculate/CalculateTest.java
+++ chapter_001/src/test/java/ru/job4j/calculate/CalculateTest.java
@@ -5,24 +5,24 @@ import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
 
 /**
-* Test.
-*
-* @author Evgeny Baranov (evgeni.baranov@gmail.com)
-* @version 1.0
-* @since 0.1
-*/
+ * Test.
+ *
+ * @author Evgeny Baranov (evgeni.baranov@gmail.com)
+ * @version 1.0
+ * @since 0.1
+ */
 
 public class CalculateTest {
 	/**
-	* Test echo.
-	*/
+	 * Test echo.
+	 */
 	@Test
-public void whenTakeNameThenThreeEchoPlusName() {
-	String input = "Evgeny Baranov";
-	String expect = "Echo, echo, echo : Evgeny Baranov";
-	Calculate calc = new Calculate();
-	String result = calc.echo(input);
-	assertThat(result, is(expect));
-}
+	public void whenTakeNameThenThreeEchoPlusName() {
+		String input = "Evgeny Baranov";
+		String expect = "Echo, echo, echo : Evgeny Baranov";
+		Calculate calc = new Calculate();
+		String result = calc.echo(input);
+		assertThat(result, is(expect));
+	}
 
 }
